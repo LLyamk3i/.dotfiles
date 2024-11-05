56a8e98f94101141e5d95c32b554eb88ada2b7a8
llyam k3i <weedtest89@gmail.com>
2024-11-05 19:03:58 +0100

    fix(nvim): fix remove of " and ' while use copy function

--- nvim/lua/custom/functions.lua
+++ nvim/lua/custom/functions.lua
@@ -2,8 +2,8 @@ local M = {}
 
 function M.sort_selected_by_length()
   -- Get the current visual selection
-  local start_pos = vim.fn.getpos("'<")
-  local end_pos = vim.fn.getpos("'>")
+  local start_pos = vim.fn.getpos "'<"
+  local end_pos = vim.fn.getpos "'>"
   local start_line = start_pos[2]
   local end_line = end_pos[2]
 
@@ -11,34 +11,32 @@ function M.sort_selected_by_length()
   local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
 
   -- Sort the lines by length
-  table.sort(lines, function(a, b) return #a < #b end)
+  table.sort(lines, function(a, b)
+    return #a < #b
+  end)
 
   -- Set the sorted lines back to the buffer
   vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, lines)
 end
 
--- Function to trim whitespace from both ends of a string
-function M.trim(s)
-  return s:match("^%s*(.-)%s*$")  -- Match and return the trimmed string
-end
-
 -- Function to copy selected text to clipboard using CopyQ
 function M.copyq()
-  -- Clear the z register
-  vim.fn.setreg('z', '')  -- Clear the contents of register z
+  -- Clear the contents of register z
+  vim.fn.setreg("z", "")
 
-  -- Yank the selected text to the unnamed register
-  vim.cmd('normal! "zy')  -- Yank the selected text into register z
+  -- Yank the selected text to the z register
+  vim.cmd 'normal! "zy'
 
   -- Get the yanked text from register z
-  local selected_text = vim.fn.getreg('z')  -- Get the contents of register z
+  local selected_text = vim.fn.getreg "z"
   if selected_text == "" then
-    print("No text selected!")
+    print "No text selected!"
     return
   end
 
-  -- Trim the selected text
-  -- selected_text = M.trim(selected_text)
+  -- Escape single and double quotes in the selected text
+  selected_text = string.gsub(selected_text, "'", "\\'")
+  selected_text = string.gsub(selected_text, '"', '\\"')
 
   -- Use CopyQ to copy the selected text
   os.execute('copyq add "' .. selected_text .. '"')
@@ -46,4 +44,3 @@ function M.copyq()
 end
 
 return M
-
