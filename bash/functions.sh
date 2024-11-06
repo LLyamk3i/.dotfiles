d9fb84522a8f9f8b0480df4b142f39aa6862ba30
llyam k3i <weedtest89@gmail.com>
2024-11-06 18:19:21 +0100

    feat(bash): add new function kebab_case

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,6 +1,10 @@
-todo ()
-{
+todo () {
   cd ~/lab/tmp/todos/
   [ ! -f TODO.$(date +%Y%m%d).txt ] && grep "\[\]" TODO.$(date -d "yesterday" +%Y%m%d).txt > TODO.$(date +%Y%m%d).txt
   nvim TODO.$(date +%Y%m%d).txt
+}
+
+kebab_case() {
+    local input_string="$1"
+    echo "$input_string" | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr -d '"'
 }
