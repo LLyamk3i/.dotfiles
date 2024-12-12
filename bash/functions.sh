0066f8352c8b9926070b4cd2c6f867cac5258e25
llyam k3i <weedtest89@gmail.com>
2024-12-12 15:25:43 +0100

    fix(bash): update 'upt' alias and allow todo func to use last file saved in todos folder

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,7 +1,25 @@
-todo () {
-  cd ~/lab/tmp/todos/
-  [ ! -f TODO.$(date +%Y%m%d).md ] && grep "\[\]" TODO.$(date -d "yesterday" +%Y%m%d).md > TODO.$(date +%Y%m%d).md
-  nvim TODO.$(date +%Y%m%d).md
+. $LOG_MESSAGE_PATH;
+
+todo() {
+  local todo_dir="~/lab/tmp/todo/";
+  cd "$todo_dir" || { print_message error "Failed to change directory to $todo_dir"; return 1; }
+
+  local today_file="TODO.$(date +%Y%m%d).md"
+  local yesterday_file="TODO.$(date -d "yesterday" +%Y%m%d).md"
+  
+  if [[ -f "$yesterday_file" ]]; then
+    grep "[]" "$yesterday_file" > "$today_file"
+  else
+    local latest_file=$(ls -t TODO.*.md 2>/dev/null | head -n 1)
+    if [[ -n "$latest_file" ]]; then
+      grep "[]" "$latest_file" > "$today_file"
+    else
+      print_message info "No TODO files found. Creating an empty TODO file."
+      touch "$today_file"
+    fi
+  fi
+
+  nvim "$today_file"
 }
 
 kebab_case() {
