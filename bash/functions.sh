53d6bde4a1d4b6df4034bcfa411c7e222ce2a73c
llyam k3i <weedtest89@gmail.com>
2024-12-13 15:01:23 +0100

    fix(nvim): skip grep if today's file exists

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,18 +1,28 @@
 . $LOG_MESSAGE_PATH;
 
 todo() {
-  local todo_dir="~/lab/tmp/todos/";
-  cd "$todo_dir" || { print_message error "Failed to change directory to $todo_dir"; return 1; }
+  local todo_dir=~/lab/tmp/todos/;
+  cd "$todo_dir" 2>/dev/null || { print_message error "Failed to change directory to $todo_dir"; return 1; }
 
-  local today_file="TODO.$(date +%Y%m%d).md"
-  local yesterday_file="TODO.$(date -d "yesterday" +%Y%m%d).md"
-  
-  if [[ -f "$yesterday_file" ]]; then
-    grep "[]" "$yesterday_file" > "$today_file"
+local today_file="TODO.$(date +%Y%m%d).md"
+local yesterday_file="TODO.$(date -d "yesterday" +%Y%m%d).md"
+
+if [[ -f "$today_file" ]]; then
+    print_message info "Today's TODO file already exists."
+    nvim "$today_file"
+    return 0
+fi
+
+if [[ -f "$yesterday_file" ]]; then
+    print_message info "found $yesterday_file";
+
+    grep "\[\]" "$yesterday_file" > "$today_file"
   else
     local latest_file=$(ls -t TODO.*.md 2>/dev/null | head -n 1)
     if [[ -n "$latest_file" ]]; then
-      grep "[]" "$latest_file" > "$today_file"
+      print_message info "found $latest_file";
+
+      grep "\[\]" "$latest_file" > "$today_file"
     else
       print_message info "No TODO files found. Creating an empty TODO file."
       touch "$today_file"
